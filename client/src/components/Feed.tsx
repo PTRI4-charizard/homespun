61c349a8a1391a09f510d85dd859e4ed78ea5ac6
Eric Mulhern <eric.mulhern@gmail.com>
2022-03-01 14:35:46 -0500

    Built basic feed page with materialui. Began work on FeedItem component

---
+++ client/src/components/Feed.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Button from '@mui/material/Button';
+import FeedItem from './FeedItem'
+
+export default function Feed() {
+  return (
+    <div className="Feed">
+      <FeedItem />
+    </div>
+  );
+}
